a3c99df2bf54354565a7279639e1f383af1e1f5b
Sandy <sandy.congreve@cmeducations.se>
2022-04-26 17:43:41 +0200

    Added navbar & display grid

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,75 +1,73 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-
-
-
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Container = styled.div`
-padding-left: 5%;
-padding-right: 5%;
-background-color: transparent;
-position: sticky;
-overflow: hidden;
-top: 0;
-width: 100%;
-z-index: 10;
-backdrop-filter: blur(50px); 
+  padding-left: 5%;
+  padding-right: 5%;
+  background-color: transparent;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  z-index: 10;
+  backdrop-filter: blur(50px);
+
 `;
 const Wrapper = styled.div`
   height: 8em;
   padding: 1em 1em;
   display: flex;
   justify-content: space-between;
-  border-bottom: 1px solid #9B753A;
+  border-bottom: 1px solid #9b753a;
 `;
-const Left = styled.div`
-  flex: 1;
 
-`;
 const Center = styled.div`
-flex: 1;
-display: flex;
-justify-content: center;
-align-items: center;
-`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const linkStyle = {
   textDecoration: "none",
   margin: 0,
   letterSpacing: 0.5,
- 
 };
 
 const Logo = styled.img`
- max-height: 10em;
- 
-
+  max-height: 10em;
 `;
-const Right = styled.div`
-  flex: 1;
+
+
+const Menu = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: flex-end;
- 
+  flex-direction: column;
+  position: fixed;
+  right: 0;
+  top: 8em;
+  background-color: white;
+  backdrop-filter: blur(50px);
+  padding: 1.5em;
+  gap:1.5em;
+  opacity:${props => props.navbarOpen ? '100%' : '0%' };
+  transition: all 0.2s;
 `;
 
 const MenuItem = styled.p`
   font-size: 18px;
   cursor: pointer;
-  margin-left: 25px;
-  color: #9B753A;
+  color: #9b753a;
   letter-spacing: 0.05;
   padding-left: 1em;
   padding-right: 1em;
-  
 `;
 const Button = styled.button`
   align-items: center;
   background-color: transparent;
-  border: 0.1px solid #9B753A;
-  border-radius: .25rem;
+  border: 0.1px solid #9b753a;
+  border-radius: 0.25rem;
   box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
-  color: #9B753A;
+  color: #9b753a;
   cursor: pointer;
   display: inline-flex;
   font-size: 16px;
@@ -78,41 +76,38 @@ const Button = styled.button`
   line-height: 1.25;
   margin: 0;
   min-height: 3rem;
-  padding: calc(.875rem - 1px) calc(1.5rem - 1px);
-
-
-
-`
-
-
+  padding: calc(0.875rem - 1px) calc(1.5rem - 1px);
+`;
 
 function Navbar() {
-  const [navbarOpen, setNavbarOpen] = useState(false)
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-  
+  function handleToggle(){
+setNavbarOpen(!navbarOpen)
+
+  }
+
   return (
     <Container>
       <Wrapper>
-        <Left>
-      
-        </Left>
-       <Center>
-       <Link to="/" style={linkStyle}>
-        
-        <Logo src="https://i.ibb.co/kDLkfBx/Dore-Baguette-Couronne-Ico-ne-de-Cercle-Clerge-Logo-2-removebg-preview.png"></Logo>
-        </Link>
-       </Center>
-        <Right>
-        <Link to="/viner" style={linkStyle}>
+        <Center>
+          <Link to="/" style={linkStyle}>
+            <Logo src="https://i.ibb.co/kDLkfBx/Dore-Baguette-Couronne-Ico-ne-de-Cercle-Clerge-Logo-2-removebg-preview.png"></Logo>
+          </Link>
+        </Center>
+       <GiHamburgerMenu size={44} onClick={handleToggle}/>
+        <Menu navbarOpen={navbarOpen}>
+          <Link to="/viner" style={linkStyle}>
             <MenuItem>VÅRA VINER</MenuItem>
           </Link>
           <Link to="/viovin" style={linkStyle}>
-            <MenuItem >OM OSS</MenuItem>
+            <MenuItem>OM OSS</MenuItem>
           </Link>
-         <MenuItem>
-          <Button>KONTAKTA</Button>
+          <MenuItem>
+            <Button>KONTAKTA</Button>
           </MenuItem>
-        </Right>
+        </Menu>
+     
       </Wrapper>
     </Container>
   );
--- src/components/Product/ProductContainer.jsx
+++ src/components/Product/ProductContainer.jsx
@@ -5,35 +5,20 @@ import { useRecoilValue } from "recoil";
 import {mobile} from "../../responsive";
 
 const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  max-width: 100%;
-  gap: 5em;
-  margin-top: 100px;
-  ${mobile({
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    flexWrap: "wrap",
-    gap: "5em",
-  })}
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+gap: 2.5em;
+padding: 0 5%;
+
 `;
 const ProductCard = styled.div`
-flex-direction: row;
-display: flex;
-  max-width: 40%;
-  height: auto;
-  padding: 1.5em;
-  flex-grow : 1;
-  ${mobile({ flex: "40%", minWidth: "100%", flexDirection: "column", })}
+
 `;
 
 const ImageContainer = styled.div`
-width: 50%;
-height: auto;
 
-${mobile({ margin: "0px", width: "100%",})}
+
+
  
 `;
 const Image = styled.img`
@@ -44,10 +29,8 @@ object-fit: cover;
  
 `;
 const Info = styled.div`
-  z-index: 2;
-  width: 50%;
-  height: auto;
-  ${mobile({ flex: "40%", width: "100%" })}
+
+
 
 `;
 const Subtitle = styled.h4`
@@ -74,8 +57,9 @@ function ProductContainer() {
       {filteredWines.map((wine) => {
         return (
           <ProductCard key={wine.id}>
+            <ImageContainer>
             <Image src={wine.img}></Image>
-
+            </ImageContainer>
             <Info>
               <Subtitle>{wine.subtitle}</Subtitle>
               <Title>{wine.title}</Title>
