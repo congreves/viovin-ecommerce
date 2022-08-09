168f44c7758c81afb6ba6bc7e37c3a40a9aa2b52
Sandy <sandy.congreve@cmeducations.se>
2022-08-09 17:22:59 +0200

    Fixed phone links in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { Container, Wrapper, Left, Logo, Center, Right, Title, ContactItem, CompanyInfo } from "./Footer.styles";
+import {
+  Container,
+  Wrapper,
+  Left,
+  Logo,
+  Center,
+  Right,
+  Title,
+  ContactItem,
+  CompanyInfo,
+} from "./Footer.styles";
 import { Link } from "react-router-dom";
 import { MailOutlined, Phone, Room } from "@mui/icons-material";
 
@@ -20,14 +30,13 @@ function Footer() {
         <Center></Center>
         <Right>
           <Title>KONTAKT</Title>
-          <ContactItem>
-           
+          <ContactItem href="tel:+46704600560">
             <Phone style={{ marginRight: "10px" }} />
             Lisskulla Panossian: 0704600560
           </ContactItem>
-          <ContactItem>
+          <ContactItem href="tel:+46708688960">
             <Phone style={{ marginRight: "10px" }} />
-            Nathalie Panossian: 0708688960 
+            Nathalie Panossian: 0708688960
           </ContactItem>
           <ContactItem href="mailto: info@viovin.se">
             <MailOutlined style={{ marginRight: "10px" }} />
@@ -35,12 +44,8 @@ function Footer() {
           </ContactItem>
         </Right>
       </Wrapper>
-      <CompanyInfo>
-        VioVin AB 
-      </CompanyInfo>
-      <CompanyInfo>
-      © viovin.se 2022
-      </CompanyInfo>
+      <CompanyInfo>VioVin AB</CompanyInfo>
+      <CompanyInfo>© viovin.se 2022</CompanyInfo>
     </Container>
   );
 }
